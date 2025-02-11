efb604858e16fd729dc9a89d41618b4a2994be3e
caeyo <oppermancarter@gmail.com>
2025-02-11 12:57:28 -0500

    Remove hard-to option, turns out not necessary

--- simp/Main.cc
+++ simp/Main.cc
@@ -215,9 +215,7 @@ int main(int argc, char** argv)
         // Use signal handlers that forcibly quit until the solver will be able to respond to
         // interrupts:
         signal(SIGINT, SIGINT_exit);
-        if (!opt_hard_timeout) {
-            signal(SIGXCPU,SIGINT_exit);
-        }
+        signal(SIGXCPU,SIGINT_exit);
 
 
         // Set limit on CPU-time:
@@ -268,9 +266,7 @@ int main(int argc, char** argv)
         // Change to signal-handlers that will only notify the solver and allow it to terminate
         // voluntarily:
         signal(SIGINT, SIGINT_interrupt);
-        if (!opt_hard_timeout) {
-            signal(SIGXCPU,SIGINT_interrupt);
-        }
+        signal(SIGXCPU,SIGINT_interrupt);
 
         S.parsing = 0;
         if(pre/* && !S.isIncremental()*/) {
